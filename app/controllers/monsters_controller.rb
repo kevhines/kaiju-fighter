b0b4538d17f00b05b3322b3c8d2294e345c18621
Kevin <kevhines@gmail.com>
2021-03-30 17:28:20 -0400

    validates monster name can't be blank

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -16,14 +16,20 @@ class MonstersController < ApplicationController
     def create
         monster = Monster.new(monster_params)
         monster.user = current_user
-        monster.save
-        redirect_to user_path(current_user)
+        if monster.save
+            redirect_to user_path(current_user)
+        else
+            redirect_to new_monster_path, alert: monster.errors.full_messages
+        end
     end
 
     def update
         monster = Monster.find_by(id: params[:id])
-        monster.update(monster_params)
-        redirect_to user_path(current_user)
+        if monster.update(monster_params)
+            redirect_to user_path(current_user)
+        else
+            redirect_to edit_monster_path(monster), alert: monster.errors.full_messages
+        end
     end
 
     def edit
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -7,7 +7,7 @@ class Monster < ApplicationRecord
     has_many :fights_defended, foreign_key: :defender_id, class_name: "Fight"
     has_many :challengers, through: :fights_defended
 
-
+    validates :name, presence: true
 
     def all_fights
         self.fights_challenged + self.fights_defended
