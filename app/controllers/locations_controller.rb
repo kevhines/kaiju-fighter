a98122455eacc02afcb345919c9d1598c906d7ce
Kevin <kevhines@gmail.com>
2021-03-31 13:08:32 -0400

    adds location edit page, and monster edit link

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,6 +9,22 @@ class LocationsController < ApplicationController
     end
 
     def edit
+        @location = Location.find_by(id: params[:id])
     end
+
+    def update
+        location = Location.find_by(id: params[:id])
+        if location.update(location_params)
+            redirect_to location_path(location)
+        else
+            redirect_to edit_location_path(location), alert: location.errors.full_messages
+        end
+    end
+
+    private
+    def location_params
+        params.require(:location).permit(:name)
+    end
+
 
 end
