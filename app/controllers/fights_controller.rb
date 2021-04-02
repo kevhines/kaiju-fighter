3a12e35520ad36bfc4996e6cc3b9bc812ad42251
Kevin <kevhines@gmail.com>
2021-04-02 11:58:18 -0400

    fixes more fields_with_errors forms

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -18,18 +18,22 @@ class FightsController < ApplicationController
     end
 
     def create
-        monster = Monster.find_by(id: params[:monster_id])
-        if my_monster?(monster) 
-            fight = monster.fights_challenged.build(fight_params)
+        @monster = Monster.find_by(id: params[:monster_id])
+        if my_monster?(@monster) 
+            @fight = @monster.fights_challenged.build(fight_params)
+            @monsters = Monster.where("id != ?", @monster.id)
         else
-            fight = monster.fights_defended.build(fight_params)
+            @fight = @monster.fights_defended.build(fight_params)
+            @monsters = Monster.where("user_id = ?", current_user.id)
         end
-        if fight.save
-            fight.attack
-            fight.save
-            redirect_to monster_path(monster)
+        if @fight.save
+            @fight.attack
+            @fight.save
+            redirect_to monster_path(@monster)
         else
-            redirect_to new_monster_fight_path(monster), alert: fight.errors.full_messages
+            flash[:alert] = @fight.errors.full_messages
+            render :new
+            #redirect_to new_monster_fight_path(@monster), alert: @fight.errors.full_messages
         end
     end
 
