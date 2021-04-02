d4cd2378fa3ee02a076ccac55fcb925226fe9724
Kevin <kevhines@gmail.com>
2021-04-02 10:07:47 -0400

    makes username uniquess case-insensitive

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
           session[:user_id] = @user.id
-          #redirect_to user_items_path(@user)
           redirect_to root_path
         else
           @errors = @user.errors.full_messages
