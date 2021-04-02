308a2466817d7182de88f0e0bfc4509773f7c68f
Kevin <kevhines@gmail.com>
2021-04-02 17:13:10 -0400

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-kevin = User.create(username: "kevin", password: "123")
-amanda = User.create(username: "amanda", password: "123")
-cameron = User.create(username: "cameron", password: "123")
+kevin = User.create(username: "Kevin", password: "123")
+amanda = User.create(username: "Amanda", password: "123")
+cameron = User.create(username: "Cameron", password: "123")
 
 
 godzilla = Monster.create(name: "Godzilla", size: 40, user: kevin )
@@ -8,18 +8,24 @@ mothra = Monster.create(name: "Mothra", size: 20, user: amanda)
 kingkong = Monster.create(name: "King Kong", size: 20, user: cameron)
 rodan = Monster.create(name: "Rodan", size: 35, user: amanda)
 behemoth = Monster.create(name: "Behemoth", size: 30, user: kevin)
+bees = Monster.create(name: "A Lot of Bees", size: 40, user: kevin )
+tiny = Monster.create(name: "Tiny", size: 15, user: amanda)
+bubba = Monster.create(name: "Bubba", size: 75, user: cameron)
+whiskers = Monster.create(name: "Whiskers", size: 45, user: kevin)
+
+
 
 newyork = Location.create(name: "New York")
 tokyo = Location.create(name: "Tokyo")
 maplewood = Location.create(name: "Maplewood")
+skullisland = Location.create(name: "Skull Island")
 
 
-one = Fight.create(title: "First Fight", challenger_id: 1, defender_id: 2, location_id: 1)
-two = Fight.create(title: "Battle for Tokyo", defender: behemoth, challenger: rodan, location: tokyo)
-three = rodan.fights_challenged.build(title: "Rodan's Revenge", defender_id: 3, location_id: 3)
-four = rodan.fights_challenged.build(title: "Rodan's still mad", defender: behemoth, location: maplewood)
-rodan.save
-one.attack
-two.attack
-three.attack
-four.attack
+Fight.battle_royale("Round One")
+Fight.battle_royale("Revenge")
+Fight.battle_royale("Too Fast")
+Fight.battle_royale("Too Furious")
+Fight.battle_royale("PayPerView")
+Fight.battle_royale("Heavy Weights")
+Fight.battle_royale("Championship Round")
+Fight.battle_royale("Just for Fun")
